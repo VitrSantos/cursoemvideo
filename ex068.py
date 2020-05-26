86b90f9281c0e6a88ddcba5e6e2e3fe78553e34f
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-05-26 11:45:16 -0300

    Resolução do exercício 68 e correções.

--- ex068.py
+++ ex068.py
@@ -1,5 +1,6 @@
 #Exercício Python #068 - Jogo do Par ou Ímpar
 from random import randint
+cont = 0
 print("=-"*20)
 print('Vamos jogar Par ou Ímpar')
 while True:
@@ -7,20 +8,21 @@ while True:
     computador = randint(0,10)
     numjogador = int(input("Digite um valor: "))
     jogador = str(input('Par ou ímpar? ')).upper().strip()[0]
-    print(jogador)
     soma = computador + numjogador
     print(f'Você jogou {numjogador} e eu joguei {computador}, total de {soma}.')
     if jogador == 'P':
         if soma % 2 == 0:
             print('É par. Você ganhou! '
                   'Vamos jogar outra vez...')
+            cont += 1
         else:
-            print('É ímpar. GAME OVER!')
+            print(f'É ímpar. GAME OVER! você ganhou {cont} vez(es).')
             break
-    if jogador == 'I' or jogador == 'Í':
+    elif jogador == 'I' or jogador == 'Í':
         if soma % 2 != 0:
             print('É ímpar. Você ganhou! '
                   'Vamos jogar outra vez...')
+            cont += 1
         else:
-            print('É par. GAME OVER!')
+            print(f'É par. GAME OVER! você ganhou {cont} vez(es).')
             break
