ea22a6dbc5ced4605209439f00302974c571c7d1
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-05-28 10:31:41 -0300

    exclusão

--- exaula16.py
+++
@@ -1,8 +0,0 @@
-#As Tuplas são variáveis compostas, que aceitam diferentes tipos de dados (números ou letras) e são indicadas por parenteses(). EX:
-tupla = ('elemento1', 'elemento2', 'elemento3', 'elemento4')
-print(tupla)
-#Caso queira escolher o elemento a ser impresso na tela, a posição do elemento será indicado dentro de colchetes[]. EX:
-print(tupla[2])
-# Não é possível atribuir valores à tupla durante a execução do programa, somente durante a declaração da mesma. EX:
-tupla[3] = 'elemento5'
-#
