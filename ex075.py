b3ab9622be533c7d5a50504eb04d131d2be4d21f
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-06-01 07:19:54 -0300

    Resolução exercício 75

--- ex075.py
+++ ex075.py
@@ -1,13 +1,14 @@
+#Exercício Python #075 - Análise de dados em uma Tupla
 qtd9 = loc3 = pares = 0
 numeros = int(input('Digite um algarismo: ')), int(input('Digite outro algarismo: ')), int(input('Digite mais um algarismo: ')), int(input('Digite o útimo algarismo: '))
-print(f'Você digitou os valores: {numeros}')
+print('=-'*20)
+print(f'Os valores digitados foram {numeros}')
+print((f'O algarismo 9 apareceu {numeros.count(9)} vezes'))
+if 3 in numeros:
+    print(f'O algarismo 3 apareceu na {numeros.index(3)}ª posição')
+else:
+    print('O algarismo 3 não foi digitado em nenhuma posição')
+print( f'Os valores pares digitados foram', end=' ' )
 for c in numeros:
-    if c == 9:
-        qtd9 += 1
-    if c == 3:
-        loc3 += numeros.index(3)+1
     if c % 2 == 0:
-        pares += 1
-print(pares)
-print(loc3)
-print(qtd9)
+        print(c, end=' ')
