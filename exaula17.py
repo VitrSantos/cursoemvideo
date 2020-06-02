4f4c951f732342b6472fcd6a7e1736f586444160
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-06-02 08:25:52 -0300

    Anotações da aula 17

---
+++ exaula17.py
@@ -0,0 +1,9 @@
+Listas
+# As listas são estruturas similares às tuplas, mas são indicadas por colchetes[].
+#Além disso, as listas apresentam a característica de serem mutáveis ao contrário das tuplas
+Há comandos como:
+lista.append(objeto) #(adiciona ao começo da lista)
+lista.insert(posição,objeto) #(adiciona em posições ao longo da lista)
+del lista[posição] #deleta itens em determinada posição
+lista.pop(posição) #deleta itens, quando em branco, apaga o ultimo elemento
+lista.remove(conteudo) #apaga os itens de acordo com o conteúdo, independente da posição
