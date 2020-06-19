39dcd02d392f5ac9d60d2955e81c5909295b54c8
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-06-19 09:37:44 -0300

    Resolução do exercício 6

--- ex006.py
+++ ex006.py
@@ -1,4 +1,3 @@
-n1 = float(input('Primeira nota: '))
-n2 = float(input('Segunda nota: '))
-s = n1 + n2
-print('A média da nota do alune é {}' .format(s / 2))
+#Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+num = int(input('Digite um número: '))
+print(f'O dobro desse número é {(num)*2}.\nO triplo desse número é {(num)*3}.\nA raiz quadrada desse número é {(num ** (1/2)):.2f}.')
