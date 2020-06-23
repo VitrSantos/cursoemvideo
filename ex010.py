ddaabffa22cc153a3cf3f8ec830e6a2a8f91d228
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-06-23 07:51:31 -0300

    Resolução do exercício 10

--- ex010.py
+++ ex010.py
@@ -1,2 +1,3 @@
-d = float(input('Quanto de dinheiro você tem na carteira? R$: '))
-print('Com',d ,'R$, você pode comprar \033[31mUS$ {:.2f}\033[m'.format(d / 5.07))
+#Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+dinheiro = float(input("Quanto dinheiro você tem no momento (reais)? "))
+print(f'Com R${dinheiro:.2f} você pode comprar {dinheiro/3.27:.2f} Dólares.')
