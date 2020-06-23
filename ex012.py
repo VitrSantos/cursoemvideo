3459464438449a509679af75b163bf7ac547b157
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-06-23 08:15:18 -0300

    Resolução do exercício 11

--- ex012.py
+++ ex012.py
@@ -1,2 +1,3 @@
-p = float(input('Qual o preço do produto? '))
-print('O preço do seu produto é de', p,'.Sendo assim, com desconto de 5% o novo preço será de {:.2f}'.format(p * 0.95))
+#Exercício Python 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+preço = (float(input('Qual o preço do produto? R$ ')))
+print(f'Com 5% de desconto seu produto custará \033[032m{preço * 0.95}')
