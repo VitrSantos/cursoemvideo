ff7db910ebd2def3b6e4a9c5b2882c1e7b8dd82b
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-06-24 08:19:36 -0300

    Resolução do exercício 18

--- ex018.py
+++ ex018.py
@@ -1,6 +1,8 @@
-import math
-a = int(input('digite um ângulo: '))
-ar = math.radians(a)
-print('O Seno do angulo de {} é igual a {:.2f}'.format(a, math.sin(ar)))
-print('O Cosseno do ângulo de {} é de {:.2f}'.format(a, math.cos(ar)))
-print('A tangente do ângulo de {} é de {:.2f}'.format(a, math.tan(ar)))
+#Exercício Python 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+from math import radians, sin, cos, tan
+ang = int(input('Digite o valor do ângulo: '))
+ran = radians(ang)
+print(f'O Seno do angulo é {sin(ran):.2f}.')
+print(f'O Cosseno do angulo é {cos(ran):.2f}')
+print(f'A tangente do ângulo é {tan(ran):.2f}')
+
