1e7c8eeb2b5095c623f0768c22db64480fba7743
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-01 12:27:17 -0300

    Resolução do exercício 19

--- ex019.py
+++ ex019.py
@@ -1,9 +1,9 @@
-import random
-n1 = input('digite um nome: ')
-n2 = input('digite um nome: ')
-n3 = input('digite um nome: ')
-n4 = input('digite um nome: ')
-lista = [n1, n2, n3, n4]
-escolhido = random.choice(lista)
-print('O escolhido foi {}'.format(escolhido))
-
+#Exercício Python 19: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+from random import choice
+n1= str(input('Nome do aluno: '))
+n2 = str(input('Nome de outro aluno: '))
+n3 = str(input('Nome de mais um aluno: '))
+n4 = str(input('Nome do último aluno: '))
+escolha = [n1, n2, n3, n4]
+escolhido = choice(escolha)
+print(escolhido)
