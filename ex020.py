a01ff8aafa4bebc4595cb5e4df8cd5d68645235a
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-01 12:27:30 -0300

    Resolução do exercício 20

--- ex020.py
+++ ex020.py
@@ -1,8 +1,9 @@
-n = input('digite o nome de um aluno: ')
-n1 = input('digite o nome de um aluno: ')
-n2 = input('digite o nome de um aluno: ')
-n3 = input('digite o nome de um aluno: ')
-l = [n, n1, n2, n3]
-import random
-o = random.shuffle(l)
-print('A ordem de apresentação dos trabalhos será {}'.format(l))
+#Exercício Python 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+n1 = str(input('Nome do aluno: '))
+n2 = str(input('Nome do aluno: '))
+n3 = str(input('Nome do aluno: '))
+n4 = str(input('Nome do aluno: '))
+from random import shuffle
+lista = [n1, n2, n3, n4]
+ordem = shuffle(lista)
+print(lista)
