e2c13667a8ee30d35fc5dc185a22c527a812a08f
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-09 08:06:58 -0300

    Resolução do exercício 26

--- ex026.py
+++ ex026.py
@@ -1,7 +1,5 @@
-f = str(input('Digite sua frase:')).upper().strip()
-na = f.count('A')
-print(na)
-print('A primeria letra A apareceu a na posição {}'.format(f.find('A')+1))
-print('A última letra A aparece na posição {}'.format(f.rfind('A')+1))
-
-
+#Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+frase = str(input('Digite uma frase: ')).upper().strip()
+print(f"Há um total de {frase.count( 'A' )} 'A' nessa frase")
+print(f'O primeiro A está na posição {frase.find("A")+1}')
+print(f'O último A está na posição {frase.rfind("A")+1}')
