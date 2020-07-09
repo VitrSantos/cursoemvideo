389f23c1ff1f181edbeb49d58f6f8b52a146c59b
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-09 07:43:27 -0300

    Resolução do exercício 25

--- ex025.py
+++ ex025.py
@@ -1,3 +1,5 @@
-n = str(input('Digite seu nome completo: ')).strip()
-print('SILVA' in n.upper())
-print(n)
+#Exercício Python 25: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+nome = str(input('Qual é seu nome? ')).upper()
+pos = nome.find('SILVA')
+fim = pos + 4
+print(f'Seu nome tem Silva? {nome[pos:pos + 5] == "SILVA"}')
