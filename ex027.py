ee8352e985f3263fcd6589cb6bf1f532e55f334b
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-09 08:18:52 -0300

    Resolução do exercício 27

--- ex027.py
+++ ex027.py
@@ -1,3 +1,4 @@
-n = str(input('Digite seu nome completo: ')).upper().strip()
-l = n.split()
-print('Muito prazer em te conhecer {} {}'.format(l[0], l[len(l)-1]))
+#Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+nome = str(input('Digite o seu nome completo: ')).upper().split()
+print(f'O seu primeiro nome é {nome[0]}')
+print(f'O seu último nome é {nome[len(nome)-1]}')
