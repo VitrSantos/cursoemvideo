188a5b9e8f1201120a363ac7d249a4eb39730740
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-09 06:39:44 -0300

    Resolução do exercício 22

--- ex022.py
+++ ex022.py
@@ -1,6 +1,12 @@
-n = input('Digite seu nome completo: ')
-nome = n.split()
-print(n.upper())
-print(n.lower())
-print(len(n.strip()))
-print(len(nome[0].strip()))
+#Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
+#– O nome com todas as letras maiúsculas e minúsculas.
+#– Quantas letras ao todo (sem considerar espaços).
+#– Quantas letras tem o primeiro nome.
+nome = str(input('Qual é o seu nome? '))
+nomesemespaço = nome.strip()
+nomes = nome.split()
+print('Analisando o seu nome...')
+print(f'Seu nome em maiúsculas é {nome.upper()}')
+print( f'Seu nome em minúsculas é {nome.lower()}')
+print(len(nome) - (nome.count(' ')))
+print(len(nomes[0]))
