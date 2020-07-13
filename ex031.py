f57ab8878923a3241fa9df707ad8314e3919fa73
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-13 14:39:07 -0300

    Resolução do exercício 30

--- ex031.py
+++ ex031.py
@@ -1,7 +1,10 @@
-k = float(input('Quantos quilômetros terá sua viagem? '))
-print('Você está prestes a começar uma viagem de {:.2f}km'.format(k))
-p = k * 0.50 if k <= 200 else k * 0.45
-print('E o preço da sua passagem será de {:.2f}'.format(p))
-
-
+#Exercício Python 31: Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
+distância = float(input('Qual é a distância(Km) da sua viagem? '))
+tarifa = 0
+if distância <= 200:
+    tarifa = 0.5
+else:
+    tarifa = 0.45
+print(f'Você está prestes a fazer uma viagem de {distância}\n'
+      f'E o valor da sua passagem será de R$ {(distância * tarifa):.2f}')
 
