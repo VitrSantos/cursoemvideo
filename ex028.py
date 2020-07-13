24853c2e3783515cb2dbdde6a776ae5254ef6a9a
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-13 13:49:20 -0300

    Resolução do exercício 28

--- ex028.py
+++ ex028.py
@@ -1,15 +1,13 @@
-print('-=-' * 55)
-print('Vou pensar em um número de 0 a 5, tente adivinhar!')
-print('-=-' * 55)
+#Exercício Python 28: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 from random import choice
-from time import sleep
-l = [0, 1, 2, 3, 4, 5,]
-e = choice(l)
-n1 = int(input('Qual número você chuta? '))
-print('Processando...')
-sleep(2)
-if n1 == e:
-    print('Parabéns, você acertou!.')
+num = 0, 1, 2, 3, 4, 5
+computador = choice(num)
+print('=-'*27)
+print('Vou pensar em um número entre  0 e 5, tente adivinhar!')
+print('=-'*27)
+jogador = int(input('Qual o seu palpite? '))
+print(f'O número em que pensei foi {computador}')
+if jogador == computador:
+    print('Você ganhou, parabéns!!!')
 else:
-    print('Ganhei, o número que eu pensei foi {}'.format(e))
-
+    print('Você perdeu, tente novamente!')
