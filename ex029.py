06a1f73c973e93f7d5da4310e7c0a504f5ba4704
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-13 14:17:00 -0300

    Resolução do exercício 29

--- ex029.py
+++ ex029.py
@@ -1,7 +1,9 @@
-v = float(input('Qual a velocidade do carro (km/h)? '))
-m = v - 80
-vm = m * 7
-if v > 80:
-    print('MULTADO! você estava dirigindo acima do limite de velocidade que é de 80km/h')
-    print('O valor a ser pago pela multa é de R${:.2f}'.format(vm))
-print('Tenha um bom dia e dirija com segurança!')
+#Exercício Python 29: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+velo = float(input('Qual a velocidade atual do carro (Km/h)? '))
+print()
+if velo > 80:
+    print('Multado! Você excedeu o limite permitido que é de 80 Km/h.')
+    print(f'O valor a ser pago pela infração é: R$ {(velo - 80)* 7:.2f}')
+else:
+    print('Você está dentro do limite.')
+print('Dirija com segurança!')
