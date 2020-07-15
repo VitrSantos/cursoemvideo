2771aba5c68683f972d73e513bffaf147a5f6235
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-15 06:58:57 -0300

    Resolução do exercício 32

--- ex032.py
+++ ex032.py
@@ -1,8 +1,10 @@
-from datetime import date
-a = int(input('Que ano você quer analizar? '))
-if a == 0:
-    a = date.today().year
-if a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
-    print('O ano {} é BISSEXTO'.format(a))
+#Exercício Python 32: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+import datetime
+atual = datetime.date.today().year
+ano = int(input('Digite um ano para verificar se ele é bissexto: '))
+if ano == 0:
+    ano = atual
+if ano % 4 == 0 or ano % 400 == 0:
+    print(f"O ano de {ano} é bissexto!")
 else:
-    print('O ano {} informado não é BISSEXTO'.format(a))
+    print(f'O ano de {ano} não é bissexto.')
