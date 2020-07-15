fe3498cd14bc0a5b79e7980d3699458310665313
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-15 07:25:54 -0300

    Resolução do exercício 34

--- ex034.py
+++ ex034.py
@@ -1,5 +1,7 @@
-x = float(input('Qual o valor do salário do funcionário? '))
-if x >=1251:
-    aumento = x * 1.10
-else: aumento = x * 1.15
-print('O valor do novo salário será R$ {:.2f}'.format(aumento))
+#Exercício Python 34: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+salario = float(input('Qual o salário do funcionário? R$ '))
+if salario <= 1250:
+    print(f'Um funcionário que recebia R$ {salario:.2f} passará a receber R$ {(salario*1.15):.2f}.')
+else:
+    print(f'Um funcionário que recebia R$ {salario:.2f} passará a receber R$ {(salario*1.1):.2f}.')
+
