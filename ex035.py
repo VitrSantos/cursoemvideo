45206eeafd875fdbf72476e0afb860ef4538f25b
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-17 09:55:30 -0300

    Resolução do exercício 35

--- ex035.py
+++ ex035.py
@@ -1,9 +1,7 @@
 #Exercício Python 35: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 print(20*"-=")
 print('Analizador de triângulos')
-print(20*"-=")9
-8
-7
+print(20*"-=")
 x = float(input('Digite o cumprimento da primeira reta: '))
 y = float(input('Digite o cumprimento da segunda reta: '))
 z = float(input('Digite o cumprimento da terceira reta: '))
