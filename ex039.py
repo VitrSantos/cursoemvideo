ef87004a2e477487cb9ebbd4c00b83f3a431b57c
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-17 14:05:02 -0300

    Resolução do exercício 39

--- ex039.py
+++ ex039.py
@@ -1,22 +1,21 @@
-from datetime import date
-ano = int(input('Ano de nascimento: '))
-idade = date.today().year - ano
-pred = idade - 18
-pred2 = 18 - idade
-alistamento = date.today().year - pred
-futuro = date.today().year + pred2
-sexo = print('''Você é homem ou mulher? 
-M = Mulher
-H = Homem''')
-opç = input('Opção ').upper()
-print('Sua idade atual é de {} anos.'.format(idade))
-if opç == 'H':
-    if idade > 18:
-        print('Você já deveria ter se alistado há {} ano(s)'.format(pred))
-        print('Seu alistamento foi em {}'.format(alistamento))
-    elif idade == 18:
-        print('Seu alistamento deve acontecer IMEDIATAMENTE!')
+#Exercício Python 39: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+import datetime
+nascimento = int(input('Em qual ano você nasceu? '))
+sexo = int(input('Qual é o seu sexo\n[1] Feminino\n[2] Masculino\nOpção: '))
+ano = datetime.datetime.today().year
+if sexo == 2:
+    if ano - nascimento == 18:
+        print('Você tem que se alistar IMEDIATAMENTE.')
+    elif ano - nascimento < 18:
+        falta = ((ano - nascimento) - 18)
+        print(f'Ainda não está na hora de se alistar. Seu alistamento irá ocorrer daqui a {-falta} ano(s), em {ano - falta}.')
     else:
-        print('Seu alistamento ocorrerá em {}'.format(futuro))
-if opç == 'M':
-    print('O alistamento militar feminino não é obrigatório no Brasil e não tem data definida. Você pode se voluntariar procurando a junta de serviço militar mais próxima.')
+        passou = ((ano - nascimento) - 18)
+        print(f'O momento de seu alistamento já passou. Você deveria ter se alistado há {passou} ano(s), em {ano - passou}.\nDirija-se a uma junta de serviço militar o mais rápido possível!')
+if sexo == 1:
+    if ano - nascimento >= 18:
+        print('O alistamento militar não é obrigatório para mulheres no Brasil.\nMas você já tem idade para se voluntariar.')
+    else:
+        falta = ((ano - nascimento) - 18)
+        print(f'Ainda não está na hora de se alistar. Seu alistamento pode ocorrer daqui a {-falta} ano(s), em {ano - falta}.' )
+
