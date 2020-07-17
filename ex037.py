487275f2815e76ff455f7ac6792b7135aee292f7
VITOR SANTOS <63022943+VitrSantos@users.noreply.github.com>
2020-07-17 10:21:23 -0300

    Resolução do exercício 37

--- ex037.py
+++ ex037.py
@@ -2,10 +2,10 @@
 num = int(input('Digite um número inteiro: '))
 base = int(input('Para qual base você deseja convertê-lo?\n[1] Binária\n[2] Octal\n[3] Hexadecimal\nOpção: '))
 if base == 1:
-    print(f'{num} convertido para BINÁRIO é igual a {bin(num)}.')
+    print(f'{num} convertido para BINÁRIO é igual a {bin(num)[2:]}.')
 elif base == 2:
-    print(f'{num} convertido para OCTAL é igual a {oct(num)}.')
+    print(f'{num} convertido para OCTAL é igual a {oct(num)[2:]}.')
 elif base == 3:
-    print(f'{num} convertido para HEXADECIMAL é igual a {hex(num)}.')
+    print(f'{num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}.')
 else:
     print('Opção inválida, tente novamente!')
